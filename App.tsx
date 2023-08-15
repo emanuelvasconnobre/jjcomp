34e6ae52fad6cfb809db35456f6db116a3809c07
nycolasmv <pedro.rocha@mv.com.br>
2023-08-15 11:58:44 -0300

    Adicionando tela de pesquisa

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from './src/pages/Login';
-import PreOrder from './src/pages/PreOrder';
+import { Login, PreOrder, Search } from './src/pages';
+import { Tabs } from 'expo-router';
 
 export default function App() {
   const Stack = createNativeStackNavigator();
@@ -18,6 +18,7 @@ export default function App() {
         >
           <Stack.Screen name="login" component={Login} />
           <Stack.Screen name="preorder" component={PreOrder} />
+          <Stack.Screen name="search" component={Search} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
